c0c53bbd148cdffda2559a7b309ae03fc719447c
MCprotein <chss3339@gmail.com>
2023-09-11 23:21:36 +0900

    클라이언트 서버 연결중. 클라이언트 환경변수 적용 전

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,39 +1,40 @@
-import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
 
 interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+  userId: number
+  id: number
+  title: string
+  body: string
 }
 
-
 function TestPage() {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [posts, setPosts] = useState<Post[]>([])
+
+  const serverHost = process.env.REACT_APP_SERVER_HOST
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+        const response = await fetch(process.env.SERVER_HOST)
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Network response was not ok')
         }
-        const data = await response.json();
-        setPosts(data);
+        const data = await response.json()
+        setPosts(data)
       } catch (error) {
-        console.error((error as Error).message);
+        console.error((error as Error).message)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
     <Container>
       <h1>테스트페이지입니다만?</h1>
       <PostList>
-        {posts.map(post => (
+        {posts.map((post) => (
           <PostItem key={post.id}>
             <h2>{post.title}</h2>
             <p>{post.body}</p>
@@ -41,24 +42,22 @@ function TestPage() {
         ))}
       </PostList>
     </Container>
-  );
+  )
 }
-
 
 //styled-components 스타일링 작업
 const Container = styled.div`
   font-family: Arial, sans-serif;
   text-align: center;
-`;
+`
 
 const PostList = styled.ul`
   list-style: none;
   padding: 0;
-`;
+`
 
 const PostItem = styled.li`
   margin-bottom: 20px;
-`;
+`
 
-
-export default TestPage;
+export default TestPage
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,16 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+
+  async getHello(): Promise<any> {
+    return await lastValueFrom(
+      this.httpService
+        .get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+        .pipe(map((response) => response.data)),
+    );
   }
 }
